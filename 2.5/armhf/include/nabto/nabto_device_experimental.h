de228bae4ca3924e35394ecaae84b015ba94a6d2
Michael Madsen <mkm@nabto.com>
2019-07-24 18:24:30 +0200

    armhf library of current code

---
+++ 2.5/armhf/include/nabto/nabto_device_experimental.h
@@ -0,0 +1,13 @@
+#ifndef _NABTO_DEVICE_EXPERIMENTAL_H_
+#define _NABTO_DEVICE_EXPERIMENTAL_H_
+
+#include "nabto_device.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+} // extern c
+#endif
+
+#endif

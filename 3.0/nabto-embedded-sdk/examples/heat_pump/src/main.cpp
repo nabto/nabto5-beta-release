76d1c382f3e4c6d2a1583d3d4109b6f7fef418b2
Michael Madsen <mkm@nabto.com>
2019-08-19 21:18:41 +0200

    examples and linux embedded sdk

---
+++ 3.0/nabto-embedded-sdk/examples/heat_pump/src/main.cpp
@@ -0,0 +1,215 @@
+#include "heat_pump.hpp"
+#include "heat_pump_config.hpp"
+#include "heat_pump_iam_policies.hpp"
+#include "heat_pump_coap.hpp"
+
+#include <nabto/nabto_device.h>
+#include <nabto/nabto_device_experimental.h>
+
+#include <cxxopts.hpp>
+
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * The first time the heatpump is started init is called and writes a
+ * configuration file. The configuration file is used in subsequent
+ * runs of the heatpump.
+ */
+
+void my_handler(int s){
+    printf("Caught signal %d\n",s);
+}
+
+bool init_heat_pump(const std::string& configFile, const std::string& productId, const std::string& deviceId, const std::string& server);
+void run_heat_pump(const std::string& configFile);
+
+int main(int argc, char** argv) {
+    cxxopts::Options options("Heat pump", "Nabto heat pump example.");
+
+    options.add_options("General")
+        ("h,help", "Show help")
+        ("i,init", "Initialize configuration file")
+        ("c,config", "Configuration file", cxxopts::value<std::string>()->default_value("heat_pump_device.json"))
+        ("log-level", "Log level to log (error|info|trace|debug)", cxxopts::value<std::string>()->default_value("info"))
+        ("log-file", "File to log to", cxxopts::value<std::string>()->default_value("heat_pump_device_log.txt"));
+
+    options.add_options("Init Parameters")
+        ("p,product", "Product id", cxxopts::value<std::string>())
+        ("d,device", "Device id", cxxopts::value<std::string>())
+        ("s,server", "hostname of the server", cxxopts::value<std::string>());
+
+    try {
+
+        auto result = options.parse(argc, argv);
+
+        if (result.count("help"))
+        {
+            std::cout << options.help() << std::endl;
+            exit(0);
+        }
+
+        if (result.count("init") > 0) {
+            std::string configFile = result["config"].as<std::string>();
+            std::string productId = result["product"].as<std::string>();
+            std::string deviceId = result["device"].as<std::string>();
+            std::string server = result["server"].as<std::string>();
+            if (!init_heat_pump(configFile, productId, deviceId, server)) {
+                std::cerr << "Initialization failed" << std::endl;
+            }
+        } else {
+            std::string configFile = result["config"].as<std::string>();
+            run_heat_pump(configFile);
+        }
+    } catch (const cxxopts::OptionException& e) {
+        std::cout << "Error parsing options: " << e.what() << std::endl;
+        std::cout << options.help() << std::endl;
+        exit(-1);
+    } catch (const std::domain_error& e) {
+        std::cout << "Error parsing options: " << e.what() << std::endl;
+        std::cout << options.help() << std::endl;
+        exit(-1);
+    }
+    return 0;
+}
+
+bool init_heat_pump(const std::string& configFile, const std::string& productId, const std::string& deviceId, const std::string& server)
+{
+    if (heat_pump_config_exists(configFile)) {
+        std::cerr << "The config already file exists, remove " << configFile << " and try again" << std::endl;
+        exit(2);
+    }
+
+    json config;
+
+    NabtoDevice* device = nabto_device_new();
+    NabtoDeviceError ec;
+
+    char* str;
+    ec = nabto_device_experimental_util_create_private_key(device, &str);
+    std::string privateKey(str);
+    nabto_device_string_free(str);
+
+    if (ec) {
+        std::cerr << "Error creating private key" << std::endl;
+        return false;
+    }
+
+
+    config["PrivateKey"] = privateKey;
+    config["ProductId"] = productId;
+    config["DeviceId"] = deviceId;
+    config["Server"] = server;
+    config["HeatPump"]["Mode"] = "COOL";
+    config["HeatPump"]["Power"] = false;
+    config["HeatPump"]["Target"] = 22.3;
+    config["HeatPump"]["Temperature"] = 21.2;
+
+
+    std::vector<uint8_t> iamCbor = json::to_cbor(defaultHeatPumpIam);
+    std::cout << "iam size " << iamCbor.size() << std::endl;
+
+    // test the iam config
+    if (nabto_device_iam_load(device, iamCbor.data(), iamCbor.size()) != NABTO_DEVICE_EC_OK) {
+        std::cerr << "Error loading default iam" << std::endl;
+        return false;
+    }
+
+    config["Iam"] = defaultHeatPumpIam;
+
+    std::string tmpFile = "tmp.json";
+    heat_pump_save_config(configFile, tmpFile, config);
+
+    nabto_device_free(device);
+
+    return true;
+}
+
+void run_heat_pump(const std::string& configFile)
+{
+    NabtoDeviceError ec;
+    json config;
+    if (!heat_pump_load_config(configFile, config)) {
+        std::cerr << "The config file " << configFile << " does not exists, run with --init to create the config file" << std::endl;
+        exit(-1);
+    }
+
+    NabtoDevice* device = nabto_device_new();
+
+    auto productId = config["ProductId"].get<std::string>();
+    auto deviceId  = config["DeviceId"].get<std::string>();
+    auto server = config["Server"].get<std::string>();
+    auto privateKey = config["PrivateKey"].get<std::string>();
+    auto iam = config["Iam"];
+
+
+    ec = nabto_device_set_product_id(device, productId.c_str());
+    if (ec) {
+        std::cerr << "Could not set product id" << std::endl;
+    }
+    ec = nabto_device_set_device_id(device, deviceId.c_str());
+    if (ec) {
+        std::cerr << "Could not set device id" << std::endl;
+    }
+    ec = nabto_device_set_server_url(device, server.c_str());
+    if (ec) {
+        std::cerr << "Could not set server url" << std::endl;
+    }
+    ec = nabto_device_set_private_key(device, privateKey.c_str());
+    if (ec) {
+        std::cerr << "Could not set private key" << std::endl;
+    }
+    std::vector<uint8_t> iamCbor = json::to_cbor(iam);
+
+    ec = nabto_device_iam_load(device, iamCbor.data(), iamCbor.size());
+    if (ec) {
+        std::cerr << "failed to load iam" << std::endl;
+    }
+    ec = nabto_device_enable_mdns(device);
+    if (ec) {
+        std::cerr << "Failed to enable mdns" << std::endl;
+    }
+    ec = nabto_device_log_set_std_out_callback(device);
+    if (ec) {
+        std::cerr << "Failed to enable stdour logging" << std::endl;
+    }
+
+    // run application
+    ec = nabto_device_start(device);
+    if (ec != NABTO_DEVICE_EC_OK) {
+        std::cerr << "Failed to start device" << std::endl;
+        return;
+    }
+
+    char* fpTemp;
+    ec = nabto_device_get_device_fingerprint_hex(device, &fpTemp);
+    if (ec) {
+        std::cerr << "Could not get fingerprint of the device" << std::endl;
+    }
+    std::string fp(fpTemp);
+    nabto_device_string_free(fpTemp);
+
+    std::cout << "Device " << productId << "." << deviceId << " Started with fingerprint " << std::string(fp) << std::endl;
+
+    HeatPump hp(device, config, configFile);
+    hp.init();
+
+    heat_pump_coap_init(device, &hp);
+
+    // Wait for the user to press Ctrl-C
+
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = my_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    pause();
+
+    nabto_device_free(device);
+    exit(0);
+}
